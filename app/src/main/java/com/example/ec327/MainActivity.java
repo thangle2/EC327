363221ba0467a8f57056427a7f2235bb43b60f8a
Thang Le <thangle2@bu.edu>
2020-11-26 18:12:16 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ec327/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.ec327;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonsplash;
+    ImageView ivSplash;
+    Animation atg,textone,texttwo;
+    int test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        test=1;
+
+        //constructors
+
+        buttonsplash=findViewById(R.id.buttonsplash);
+        ivSplash=findViewById(R.id.logo);
+
+        //fonts
+        Typeface AR=Typeface.createFromAsset(getAssets(),"fonts/anmatic_regular.ttf");
+        Typeface AB=Typeface.createFromAsset(getAssets(),"fonts/chunkfive_print.otf");
+
+        atg= AnimationUtils.loadAnimation(this,R.anim.atg);
+        textone= AnimationUtils.loadAnimation(this,R.anim.textone);
+        texttwo= AnimationUtils.loadAnimation(this,R.anim.texttwo);
+
+        ivSplash.startAnimation(atg);
+
+        buttonsplash.startAnimation(texttwo);
+        if(test==1) {
+            buttonsplash.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent a = new Intent(MainActivity.this, Survey.class);
+                    a.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+                    startActivity(a);
+                }
+            });
+        }
+        if(test==2){
+            buttonsplash.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent a = new Intent(MainActivity.this, Home.class);
+                    a.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+                    startActivity(a);
+                }
+            });
+        }
+
+
+
+
+    }
+}
