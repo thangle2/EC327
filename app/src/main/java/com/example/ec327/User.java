8ce07a86af979375fe92fd2d2ea1caed661be72d
wondr <radhey1999.work@gmail.com>
2020-12-04 00:15:08 +0530

    Updates 2

--- app/src/main/java/com/example/ec327/User.java
+++ app/src/main/java/com/example/ec327/User.java
@@ -35,9 +35,7 @@ public class User implements Serializable {
         return lastName;
     }
 
-    public String getUsername() {
-        return username;
-    }
+ //   public String getUsername() {        return username;    }
 
     public int getAge() {
         return age;
@@ -55,9 +53,7 @@ public class User implements Serializable {
         this.lastName = lastName;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+  //  public void setUsername(String username) {        this.username = username;    }
 
     public void setAge(int age) {
         this.age = age;
